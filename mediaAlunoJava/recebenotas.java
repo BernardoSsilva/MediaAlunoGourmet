4ebae1082adebfd6df5b95db48da10b4449ec7af
Bernardo <126777966+BernardoSsilva@users.noreply.github.com>
2023-04-28 11:15:53 -0300

    Add files via upload

---
+++ mediaAlunoJava/recebenotas.java
@@ -0,0 +1,12 @@
+public class recebenotas{
+    public float notaUm, notaDois, notaTres;
+    public void receberNotaum(Float nota){
+        notaUm = nota;
+    }
+    public void receberNotadois(Float nota){
+        notaDois = nota;
+    }
+    public void receberNotatres(Float nota){
+        notaTres = nota;
+    }
+}
